d51896917062c12fe1767fac63d0e132ec34cd3a
Brian Liotti <bliotti@protonmail.com>
2021-03-09 05:16:38 -0500

    trans

--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,36 @@ import { Trans } from './Trans'
 
 export const Home = () => {
     const [aquaData, setData] = useState({})
+    const [transData, setTransData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const url = process.env.REACT_APP_BASE_URL
+
+    const fetchTransDetails = (trans) => {
+        const allRequests = trans.map((tran) =>
+            axios.get(`${url}/tx/${tran.hash}`)
+        )
+        return Promise.all(allRequests)
+    }
 
     const fetchData = async () => {
-        const url = process.env.REACT_APP_BASE_URL
         setIsLoading(true)
         if (url) {
             try {
-                const response = await axios.get(`${url}/block/latest`)
-                console.log(response)
+                const response = await axios.get(`${url}/block/362619`)
+                console.log(
+                    '🚀 ~ file: Home.js ~ line 24 ~ fetchData ~ response',
+                    response
+                )
                 setData(response.data)
                 setIsLoading(false)
+                try {
+                    const all = await fetchTransDetails(
+                        response.data[1].Transactions
+                    )
+                    setTransData(all)
+                } catch (error) {
+                    console.error(error)
+                }
             } catch (error) {
                 console.error(error)
                 setIsLoading(false)
@@ -32,14 +51,14 @@ export const Home = () => {
 
         return () => clearInterval(interval)
     }, [])
-
+    console.log(transData)
     return (
         <div className="content">
             <h3>Block</h3>
             {isLoading ? <div>Loading ...</div> : Block(aquaData[0])}
 
             <h3>Transactions</h3>
-            {isLoading ? <div>Loading ...</div> : Trans(aquaData[1])}
+            {isLoading ? <div>Loading ...</div> : Trans(aquaData[1], transData)}
         </div>
     )
 }
--- src/Trans.js
+++ src/Trans.js
@@ -1,17 +1,33 @@
-export function Trans(data) {
+export function Trans(data, transData) {
     return (
         <ul>
-            {data.Transactions.map((item) => (
-                <p key={item.hash}>
-                    <a
-                        className="App-link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={`https://explorer.aquacha.in/tx/${item.hash}`}
-                    >
-                        {item.hash}
-                    </a>
-                </p>
+            {transData.map((item, idx) => (
+                <div style={{ marginBottom: 20 }}>
+                    <p>
+                        {`${idx}:\t `}
+                        <a
+                            key={item.data[0].hash}
+                            className="App-link"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://explorer.aquacha.in/tx/${item.data[0].hash}`}
+                        >
+                            {item.data[0].hash}
+                        </a>
+                    </p>
+                    <ul>
+                        <p>to: {item.data[0].to}</p>
+                        <p>from: {item.data[1].from}</p>
+                        <p>
+                            value:
+                            {parseInt(item.data[0].value, 16).toLocaleString()}
+                        </p>
+                        <p>
+                            txFee:
+                            {parseInt(item.data[1].txFee, 16).toLocaleString()}
+                        </p>
+                    </ul>
+                </div>
             ))}
         </ul>
     )
